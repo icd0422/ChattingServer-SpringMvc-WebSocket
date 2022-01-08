2234a80e47a63513e78e6478c056c100ae920ec6
cori <cori.jang@bunjang.co.kr>
2022-01-08 15:57:00 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/chattingserverspringmvcwebsocket/ChattingServerSpringMvcWebSocketApplication.kt
@@ -0,0 +1,11 @@
+package com.example.chattingserverspringmvcwebsocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChattingServerSpringMvcWebSocketApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChattingServerSpringMvcWebSocketApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/chattingserverspringmvcwebsocket/ChattingServerSpringMvcWebSocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.chattingserverspringmvcwebsocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChattingServerSpringMvcWebSocketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
